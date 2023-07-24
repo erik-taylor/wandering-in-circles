b3842b99c5594cc8f9e1b0d7e1851e7f02bfa5c8
Erik Taylor <erik.taylor@learninga-z.com>
2023-07-24 15:53:49 -0700

    og tags, img count, -shuffle + reverse methods

--- src/app/image-gallery/image-gallery.component.ts
+++ src/app/image-gallery/image-gallery.component.ts
@@ -35,8 +35,7 @@ export class ImageGalleryComponent implements OnInit {
         let {public_id, version, format} = img;
         let imgUrl = `https://res.cloudinary.com/wanderingincircles/image/upload/c_scale,w_700/f_auto/q_auto/v${version}/${public_id}.${format}`;
         this.newImgArr.push(imgUrl);
-        this.shuffleImages();  
-        //this.reverseImages();  
+        this.reverseImages();  
       });
     } else {
       console.error(`dun f*ckd up that http response`);
