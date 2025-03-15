daf9fc99c53e3a20287d6d991fc9cc11060c7443
Erik Taylor <erik.taylor@learninga-z.com>
2025-03-15 09:13:37 -0700

    Add meta properties

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Wandering in circles coming soon!
-        </p>
+        <p>From picturesque campsites in national parks to vibrant festivals and charming towns, traveling in an Airstream allows you to immerse yourself in the beauty of diverse landscapes while enjoying the convenience of a well-appointed and stylish mobile abode.</p>
       </header>
     </div>
   );
